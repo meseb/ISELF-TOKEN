362d4153ea8a7420d7b06be00023402bf9dcd147
meseb <messoud.sebbahi@gmail.com>
2026-01-06 15:45:18 +0100

    Create iSelf token contract (fixed supply + permit)

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract ISelfToken is ERC20, ERC20Permit {
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 10 ** 18;
+
+    constructor(address treasury)
+        ERC20("iSelf", "ISELF")
+        ERC20Permit("iSelf")
+    {
+        require(treasury != address(0), "treasury=0");
+        _mint(treasury, MAX_SUPPLY);
+    }
 }
